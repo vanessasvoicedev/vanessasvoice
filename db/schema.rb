3f5efd12a028f93e04587535270c96837b4c4d96
Vanessa Voice <vanessasvoicedev@gmail.com>
2011-11-08 14:41:53 -0800

    added authorization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111108213403) do
+ActiveRecord::Schema.define(:version => 20111108223430) do
 
   create_table "images", :force => true do |t|
     t.string   "image_file_name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20111108213403) do
     t.datetime "updated_at"
     t.string   "remember_me_token"
     t.datetime "remember_me_token_expires_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["remember_me_token"], :name => "index_users_on_remember_me_token"
