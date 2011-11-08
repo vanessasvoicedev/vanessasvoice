064dce293a3a3ab10e54c552685f542838d7fc9d
Vanessa Voice <vanessasvoicedev@gmail.com>
2011-11-08 13:50:05 -0800

    added sorcery

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111108171953) do
+ActiveRecord::Schema.define(:version => 20111108213403) do
 
   create_table "images", :force => true do |t|
     t.string   "image_file_name"
@@ -31,5 +31,17 @@ ActiveRecord::Schema.define(:version => 20111108171953) do
   end
 
   add_index "pages", ["slug"], :name => "index_pages_on_slug", :unique => true
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
+  end
+
+  add_index "users", ["remember_me_token"], :name => "index_users_on_remember_me_token"
 
 end
