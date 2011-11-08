f80fd72205d31346ed556a70aaa4acc895cc4419
Vanessa Voice <vanessasvoicedev@gmail.com>
2011-11-08 10:13:38 -0800

    precompiling mercury js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,8 @@ Vanessa::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
   config.assets.precompile += %w( mercury.css )
+  config.assets.precompile += %w( mercury.js )
+  config.assets.precompile += %w( mercury_loader.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
